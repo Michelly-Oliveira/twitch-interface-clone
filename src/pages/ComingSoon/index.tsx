7e19bf83722a17f293669061a1fa813fe8706509
Michelly-Oliveira <michellyneris.oliveira@gmail.com>
2020-08-31 13:55:22 -0300

    upload files

---
+++ src/pages/ComingSoon/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container, Heading } from "./styles";
+
+const ComingSoon: React.FC = () => {
+  return (
+    <Container>
+      <Heading>Coming Soon</Heading>
+    </Container>
+  );
+};
+
+export default ComingSoon;
